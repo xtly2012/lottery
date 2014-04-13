29f7a08d9bc1a3af6703b218e4dbe96f1f4809b9
xtly2012 <xtly2012@163.com>
2014-04-13 10:07:48 +0000

    双色球线性分析
    git-svn-id: svn://127.0.0.1/lottery/trunk@21 c4401f3e-8d1c-884f-a5a6-a3fae65fe5e0

--- src/main/java/com/chen/lottery/service/LotteryAnalyzeService.java
+++ src/main/java/com/chen/lottery/service/LotteryAnalyzeService.java
@@ -4,7 +4,27 @@ import java.util.List;
 
 import com.chen.lottery.service.bo.BallCountBO;
 
+/**
+ * 双色球分析类
+ * 
+ * @project lottery
+ * @module  lottery
+ * @comments
+ * @nameSpace com.chen.lottery.service
+ * @author chen
+ * @since 2014-4-13
+ * @see
+ * @modifier
+ * @date
+ * @reason 
+ * @version
+ */
 public interface LotteryAnalyzeService
 {
+    /**
+     * 双色球彩线性分析
+     * 
+     * @return 分析结果
+     */
     List<BallCountBO> analyzeBallLine();
 }
